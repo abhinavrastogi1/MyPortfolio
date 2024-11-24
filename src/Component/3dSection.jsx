35005f295e0b23614ac6ac4ec3f505321a72fe3f
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2024-11-24 17:19:29 +0530

    added the background to the spaceman

--- src/Component/3dSection.jsx
+++ src/Component/3dSection.jsx
@@ -1,21 +1,35 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { LoadingScreen } from "./LoadingScreen";
-import { Spaceman } from "./Spaceman";
 import { OrbitControls } from "@react-three/drei";
+import { Spaceman } from "./Spaceman";
+import Stars from "../assets/1Stars.svg";
+import Planets from "../assets/2Planets.svg";
+import Moutains from "../assets/3Mountain.svg";
+import BottomMOuntains from "../assets/4Mountain.svg";
+import Crater from "../assets/5Crater.svg";
+import Sun from "../assets/6Sun.svg";
 
 function ThreeDimensionalSection() {
   return (
     <>
       {" "}
-      
-      <div className="w-full h-screen bg-black realtive">
+      <div className="w-full h-[150vh]  realtive">
+        <img src={Stars} className="z-0 absolute  w-full" />
+        <img src={Planets} className="z-0 absolute" />
+        <img src={Moutains} className="z-0 absolute " />
+        <img src={BottomMOuntains} className="z-0 absolute" />
+        <img src={Crater} className="z-0 absolute" />
+        <img src={Sun} className="z-0 absolute" />
         <Canvas className="absolute" camera={{ near: 0.1, far: 1000 }}>
-          <OrbitControls />
+          <OrbitControls enableZoom={false} />
           <Suspense fallback={<LoadingScreen />}>
-            <Spaceman />
+            <directionalLight color={0x00c3e7ff} />
+            <Spaceman depthTest={false} />
           </Suspense>
         </Canvas>
+
+        <div className="w-full h-[100vh]"></div>
       </div>
     </>
   );
--- src/Component/Spaceman.jsx
+++ src/Component/Spaceman.jsx
@@ -1,6 +1,6 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
 import spaceman from "../assets/spaceman.glb";
-import { a } from "@react-spring/three";
+
 import { useEffect, useRef } from "react";
 export function Spaceman() {
   const { scene, animations } = useGLTF(spaceman);
@@ -8,12 +8,12 @@ export function Spaceman() {
   const { actions } = useAnimations(animations, spacemanRef);
   useEffect(() => {
     actions["Idle"].play();
+    actions["Idle"].timeScale=2
   }, [animations]);
 
   return (
     <mesh>
-      <primitive object={scene} ref={spacemanRef} rotation={[2, 8, 4.7]}
-      position={[1.2,-0.9,1.7]} />
+      <primitive object={scene} ref={spacemanRef} rotation={[0,2,0]}c position={[0,-0.1,2]}/>
     </mesh>
   );
 }
